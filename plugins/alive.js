78e4958b3c13ca23877ab6c003b94a337cb8daa8
Aqibchaudhary <45809714+Aqibchaudhary@users.noreply.github.com>
2022-08-27 16:16:31 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,13 +1,13 @@
 /* Copyright (C) 2022 Sourav KL11.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-Raganork MD - Sourav KL11
+Bad Mafia - Aqib chaudhary
 */
 const {
   FancyRandom,
   getListFromCommand,
   skbuffer
-} = require("raganork-bot");
+} = require("Bad MAFIA-bot");
 const {
   Module,
   commands
